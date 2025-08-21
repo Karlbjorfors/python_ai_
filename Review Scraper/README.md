36d72ef9d6d195abbccd007b40912b0395fe8ca3
Karl Björfors <kallebjorfors@live.co.uk>
2025-08-21 10:29:47 +0200

    Add initial implementation of review scraper and related files
    - Created main.py for scraping reviews from Google Maps
    - Added README.md for setup instructions
    - Included requirements.txt for necessary packages
    - Implemented vector.py for processing restaurant reviews
    - Added binary files for data storage

---
+++ Review Scraper/README.md
@@ -0,0 +1,24 @@
+# 🛠️ Review Scraper Setup Guide
+
+## 1. Create a Virtual Environment
+
+```bash
+python -m venv venv
+```
+
+## 2. Activate the Virtual Environment
+
+```powershell
+./venv/Scripts/activate
+```
+
+---
+
+> **Tip:**
+>
+> - Make sure you have Python installed.
+> - Run the above commands in your project directory.
+
+---
+
+**Happy Coding! 🚀**
---
+++ Review Scraper/main.py
@@ -0,0 +1,26 @@
+# STEP 1 Import the libraries and configure the logging messages
+from playwright.sync_api import sync_playwright
+import pandas as pd
+import re
+import emoji
+import logging
+import time
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)S - %(message)s')
+logger = logging.getLogger(__name__)
+
+# STEP 2 Setting Up Playwright
+
+# Step 3: Navigating to Google Maps
+
+# Step 4: Clean the reviews text function
+
+# Step 5: Extracting Reviews
+
+# Step 6: Saving Data
+
+# Step 7: Running the Script
+
+#
